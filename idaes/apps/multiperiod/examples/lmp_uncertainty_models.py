9321d33a2ab2f03870a04f69a00573a1f721e7b5
jasherma <jasherma@andrew.cmu.edu>
2022-01-19 09:54:45 -0500

    Fix Hysteresis uncertainty set class

--- idaes/apps/multiperiod/examples/lmp_uncertainty_models.py
+++ idaes/apps/multiperiod/examples/lmp_uncertainty_models.py
@@ -38,32 +38,22 @@ def get_lmp_data(n_time_points, start=0):
     return prices_used[start:n_time_points + start]
 
 
-class SimpleLMPBoxSet():
-    def __init__(self, lmp_data, n_recent, growth_rate, avg_multiplier):
-        """Initialize simple LMP box set."""
+class LMPBoxSet():
+    def __init__(self, lmp_data):
         self.lmp_sig_nom = lmp_data
         self.n_time_points = len(lmp_data)
-        self.n_recent = n_recent
-        self.growth_rate = growth_rate
-        self.mov_avg_multiplier = avg_multiplier
 
     def bounds(self):
-        """Evaluate LMP uncertainty set bounds."""
-        multipliers = [self.growth_rate * t for t in range(self.n_time_points)]
+        return [(self.lmp_sig_nom[time], self.lmp_sig_nom[time])
+                for time in range(self.n_time_points)]
 
-        moving_average = [self.mov_avg_multiplier
-                          * sum([self.lmp_sig_nom[tprime]
-                                / (t - max(0, t - self.n_recent))
-                                for tprime in range(max(0, t - self.n_recent),
-                                                    t - 1)])
-                          for t in range(self.n_time_points)]
-        lmp_bounds = [(max(0, self.lmp_sig_nom[time]
-                           * (1 - multipliers[time])),
-                      max(moving_average[time], self.lmp_sig_nom[time] *
-                          (1 + multipliers[time])))
-                      for time in range(self.n_time_points)]
-
-        return lmp_bounds
+    def bounds_valid(self,):
+        bounds = self.bounds()
+        for time in range(len(bounds)):
+            lb, ub = bounds[time]
+            if lb > self.lmp_sig_nom[time] or ub < self.lmp_sig_nom[time]:
+                return False
+        return True
 
     def write_bounds(self, filename):
         """Write box set bounds to file."""
@@ -94,23 +84,71 @@ class SimpleLMPBoxSet():
         lower_bds = np.array([bounds[idx][0] for idx in range(len(bounds))])
         upper_bds = np.array([bounds[idx][1] for idx in range(len(bounds))])
 
+        color = "black"
+
         # generate the plots
-        plt.plot(upper_bds, label='upper')
-        plt.plot(self.lmp_sig_nom, label='nominal')
-        plt.plot(lower_bds, label='lower')
+        plt.plot(upper_bds, "--", color=color, label='upper', linewidth=1.0)
+        plt.plot(self.lmp_sig_nom, color=color, label='nominal', linewidth=1.8)
+        plt.plot(lower_bds, "--", color=color, label='lower', linewidth=1.0)
+        plt.fill_between(range(len(lower_bds)), lower_bds, upper_bds,
+                         color=color, alpha=0.1)
         plt.xlabel('period')
         plt.ylabel('LMP signal')
-        plt.legend()
+        # plt.legend()
 
         plt.show()
 
 
+class ConstantUncertaintyBoxSet(LMPBoxSet):
+    def __init__(self, lmp_data, uncertainty):
+        self.lmp_sig_nom = lmp_data
+        self.n_time_points = len(lmp_data)
+        self.uncertainty = uncertainty
+
+    def bounds(self):
+        bounds = [(self.lmp_sig_nom[time] - self.uncertainty,
+                   self.lmp_sig_nom[time] + self.uncertainty)
+                  for time in range(self.n_time_points)]
+        bounds[0] = (self.lmp_sig_nom[0], self.lmp_sig_nom[0])
+        return bounds
+
+
+class SimpleLMPBoxSet(LMPBoxSet):
+    def __init__(self, lmp_data, n_recent, growth_rate, avg_multiplier):
+        """Initialize simple LMP box set."""
+        self.lmp_sig_nom = lmp_data
+        self.n_time_points = len(lmp_data)
+        self.n_recent = n_recent
+        self.growth_rate = growth_rate
+        self.mov_avg_multiplier = avg_multiplier
+
+    def bounds(self):
+        """Evaluate LMP uncertainty set bounds."""
+        multipliers = [self.growth_rate * t for t in range(self.n_time_points)]
+
+        moving_average = [self.mov_avg_multiplier
+                          * sum([self.lmp_sig_nom[tprime]
+                                / (t - max(0, t - self.n_recent))
+                                for tprime in range(max(0, t - self.n_recent),
+                                                    t - 1)])
+                          for t in range(self.n_time_points)]
+        lmp_bounds = [(max(0, self.lmp_sig_nom[time]
+                           * (1 - multipliers[time])),
+                      max(moving_average[time], self.lmp_sig_nom[time] *
+                          (1 + multipliers[time])))
+                      for time in range(self.n_time_points)]
+
+        return lmp_bounds
+
+
 class HysterLMPBoxSet(SimpleLMPBoxSet):
     def __init__(self, lmp_data, n_recent, growth_rate, avg_multiplier,
-                 latency):
+                 latency, start_day_hour=0, include_peak_effects=False):
         """Initialize hysteresis LMP box set."""
         super().__init__(lmp_data, n_recent, growth_rate, avg_multiplier)
         self.latency = latency
+        self.start_day_hour = start_day_hour
+        self.include_peak_effects = include_peak_effects
 
     def bounds(self):
         """Evaluate LMP uncertainty set bounds."""
@@ -127,11 +165,11 @@ class HysterLMPBoxSet(SimpleLMPBoxSet):
         # evaluate the bounds
         lmp_bounds = []
         drop_times = [time for time in range(self.n_time_points) if
-                      self.lmp_sig_nom[time] == 0 and
+                      self.lmp_sig_nom[time] <= 0 and
                       self.lmp_sig_nom[max(0, time - 1)] > 0]
         spike_times = [time for time in range(self.n_time_points) if
                        self.lmp_sig_nom[time] > 0 and
-                       self.lmp_sig_nom[max(0, time - 1)] == 0]
+                       self.lmp_sig_nom[max(0, time - 1)] <= 0]
         spike_diffs = [self.lmp_sig_nom[time] - self.lmp_sig_nom[time - 1]
                        for time in spike_times]
         for time in range(self.n_time_points):
@@ -140,31 +178,71 @@ class HysterLMPBoxSet(SimpleLMPBoxSet):
             time_high = min(self.n_time_points, time + self.latency + 1)
             lb = max(0, self.lmp_sig_nom[time] * (1 - multipliers[time]))
             if np.any(self.lmp_sig_nom[time_low:time_high] == 0):
-                lb = 0
+                periods_since_drop = range(time_low, time)
+                if len(periods_since_drop) != 0:
+                    avg_lb = (sum(lmp_bounds[t][0] for t in periods_since_drop)
+                              / len(periods_since_drop)) * 0.7
+                else:
+                    avg_lb = 0
+                lb = min(0, -moving_avgs[time], avg_lb)
+                # lb = min(0, -moving_avgs[time])
 
-            ub = min(200,
-                     max(moving_avgs[time],
-                         self.lmp_sig_nom[time] * (1 + multipliers[time])))
+            ub = max(moving_avgs[time],
+                     self.lmp_sig_nom[time] * (1 + multipliers[time]))
             if time in drop_times:
                 diff = self.lmp_sig_nom[time - 1] - self.lmp_sig_nom[time - 2]
-                ub = lmp_bounds[time - 1][1] + diff
+                ub = max(self.lmp_sig_nom[time] + abs(diff * 0.1),
+                         lmp_bounds[time - 1][1] + diff)
             elif time + 1 in spike_times:
                 diff = spike_diffs[spike_times == time]
-                print(diff)
+                # print(diff)
                 ub = diff
+
+            # augment uncertainty during peak hours
+            if self.include_peak_effects:
+                time_of_day = (time + self.start_day_hour) % 24
+                at_sunrise = time_of_day >= 6 and time_of_day <= 8
+                at_sunset = time_of_day >= 18 and time_of_day <= 20
+                if at_sunrise or at_sunset:
+                    lb -= (self.lmp_sig_nom[time] - lb) * 0.5
+                    lb = max(0, lb) if self.lmp_sig_nom[time] > 0 else lb
+                    ub -= (self.lmp_sig_nom[time] - ub) * 0.5
 
             lmp_bounds.append((lb, ub))
 
         return lmp_bounds
 
 
+class ExpandDiffSet(LMPBoxSet):
+    def __init__(self, lmp_data, growth_rate):
+        self.lmp_sig_nom = lmp_data
+        self.growth_rate = growth_rate
+        self.n_time_points = len(self.lmp_sig_nom)
+
+    def bounds(self):
+        lower_bounds = [self.lmp_sig_nom[0]]
+        upper_bounds = [self.lmp_sig_nom[0]]
+        for time in range(1, self.n_time_points):
+            diff = self.lmp_sig_nom[time] - self.lmp_sig_nom[time - 1]
+            growth_factor = ((self.growth_rate * time)
+                             if diff <= 0
+                             else (- self.growth_rate * time))
+            lb = lower_bounds[time - 1] + diff * (1 + growth_factor)
+            ub = upper_bounds[time - 1] + diff * (1 - growth_factor)
+
+            lower_bounds.append(lb)
+            upper_bounds.append(ub)
+        
+        return [(lb, ub) for lb, ub in zip(lower_bounds, upper_bounds)]
+
+
 if __name__ == "__main__":
     # decide set parameters
-    n_periods = 41
-    start = 0
-    n_recent = 5
-    growth_rate = 0.02
-    mov_avg_mult = 0.2
+    n_periods = 24
+    start = 4000
+    n_recent = 4
+    growth_rate = 0.03
+    mov_avg_mult = 0.3
     hyster_latency = 1
 
     lmp_data = get_lmp_data(n_periods, start=start)
@@ -172,5 +250,12 @@ if __name__ == "__main__":
     # lmp_set.plot_bounds()
 
     hyster_set = HysterLMPBoxSet(lmp_data, n_recent, growth_rate, mov_avg_mult,
-                                 hyster_latency)
+                                 hyster_latency, start_day_hour=0,
+                                 include_peak_effects=True)
+    print("Bounds valid:", hyster_set.bounds_valid())
     hyster_set.plot_bounds()
+
+    constant_set = ConstantUncertaintyBoxSet(lmp_data, 10)
+    # constant_set.plot_bounds()
+    expand_set = ExpandDiffSet(lmp_data, growth_rate)
+    # expand_set.plot_bounds()
