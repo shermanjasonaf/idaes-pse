e4538aa0e24a2c621c6565a58bd983873e02f8c1
jasherma <jasherma@andrew.cmu.edu>
2022-03-04 17:06:40 -0500

    Edit docstrings and PEP8 rankine modules

--- idaes/apps/multiperiod/examples/multiperiod_rankine_cycle.py
+++ idaes/apps/multiperiod/examples/multiperiod_rankine_cycle.py
@@ -1,16 +1,36 @@
-import pyomo.environ as pyo
-import numpy as np
-from random import random
-from idaes.apps.multiperiod.multiperiod import MultiPeriodModel
-from idaes.apps.rankine.simple_rankine_cycle import create_model, set_inputs, initialize_model, close_flowsheet_loop, add_operating_cost
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Example driver for multiperiod.py using a simple rankine cycle.
+"""
+
 import os
+from random import random
+
+import numpy as np
+
+import pyomo.environ as pyo
+from pyomo.core.expr import current as EXPR
+from pyomo.common.collections import ComponentSet
+from pyomo.core.expr.visitor import identify_variables
+
+from idaes.apps.rankine.simple_rankine_cycle import degrees_of_freedom
 from idaes.core.util import to_json, from_json
-
-
-# Example driver for multiperiod.py using a simple rankine cycle
+from idaes.apps.multiperiod.multiperiod import MultiPeriodModel
+from idaes.apps.rankine.simple_rankine_cycle import (
+    create_model,
+    set_inputs,
+    initialize_model,
+    close_flowsheet_loop,
+    add_operating_cost,
+)
 
 
 def is_equality(con):
+    """
+    Determine whether a constraint is an equality by
+    checking whether bounds have the same value.
+    """
     return (
         con.lower is not None
         and con.upper is not None
@@ -23,18 +43,18 @@ def count_degrees_of_freedom(block, control_vars=None):
     Count the number of equality constraints in a block
     and state variables participating in those constraints.
     """
-    from pyomo.common.collections import ComponentSet
-    from pyomo.core.expr.visitor import identify_variables
-    from idaes.apps.rankine.simple_rankine_cycle import degrees_of_freedom
-
     if control_vars is None:
         cvar_set = ComponentSet()
     else:
         cvar_set = ComponentSet(control_vars)
 
-    cons = [con for con in block.component_data_objects(pyo.Constraint,
-            active=True, descend_into=True)
-            if is_equality(con)]
+    cons = [
+        con
+        for con in block.component_data_objects(
+            pyo.Constraint, active=True, descend_into=True
+        )
+        if is_equality(con)
+    ]
     svars = ComponentSet(
         var
         for con in cons
@@ -58,7 +78,10 @@ initialize_json_filename = "./initialized_state.json.gz"
 
 
 def create_ss_rankine_model():
-    # Create a steady-state ranking cycle model, not yet setup for multi-period
+    """
+    Create a steady-state model of the rankine cycle
+    for a single time period block.
+    """
     p_lower_bound = 300
     p_upper_bound = 450
 
@@ -71,18 +94,21 @@ def create_ss_rankine_model():
         from_json(m.rankine, fname=initialize_json_filename, gz=True)
     else:
         m.rankine = initialize_model(m.rankine)
-        to_json(m.rankine, fname=initialize_json_filename,
-                gz=True, human_read=True)
+        to_json(
+            m.rankine, fname=initialize_json_filename, gz=True, human_read=True
+        )
 
     m.rankine = close_flowsheet_loop(m.rankine)
     m.rankine = add_operating_cost(m.rankine)
 
     # Setting bounds for net cycle power output for a capex design
     m.rankine.fs.eq_min_power = pyo.Constraint(
-        expr=m.rankine.fs.net_cycle_power_output >= p_lower_bound*1e6)
+        expr=m.rankine.fs.net_cycle_power_output >= p_lower_bound * 1e6
+    )
 
     m.rankine.fs.eq_max_power = pyo.Constraint(
-        expr=m.rankine.fs.net_cycle_power_output <= p_upper_bound*1e6)
+        expr=m.rankine.fs.net_cycle_power_output <= p_upper_bound * 1e6
+    )
 
     m.rankine.fs.boiler.inlet.flow_mol[0].unfix()
     m.rankine.fs.boiler.inlet.flow_mol[0].setlb(1)
@@ -91,57 +117,85 @@ def create_ss_rankine_model():
 
     # Create a multiperiod capable steady-state rankine cycle model. This is a
     # user-provided function to a MultiPeriod class
+
+
 def create_mp_rankine_block(lmp_signal):
     """
+    Create the block for a single period of the multi-period
+    rankine cycle model.
+
+    Arguments
+    ---------
         lmp_signal: price for time period
     """
     m = create_ss_rankine_model()
     b1 = m.rankine
 
-    #Add new coupling variable for next time
-    b1.power_output = pyo.Expression(expr = b1.fs.net_cycle_power_output*1e-6)
-    b1.next_power_output = pyo.Var(within=pyo.NonNegativeReals,bounds = (300,450), initialize=1.5)
+    # Add new coupling variable for next time
+    b1.power_output = pyo.Expression(expr=b1.fs.net_cycle_power_output * 1e-6)
+    b1.next_power_output = pyo.Var(
+        within=pyo.NonNegativeReals, bounds=(300, 450), initialize=1.5
+    )
 
-    #Add ramping constraint
-    b1.ramp1 = pyo.Constraint(expr=b1.power_output - b1.next_power_output <= 100)
-    b1.ramp2 = pyo.Constraint(expr=b1.next_power_output - b1.power_output <= 100)
+    # Add ramping constraint
+    b1.ramp1 = pyo.Constraint(
+        expr=b1.power_output - b1.next_power_output <= 100
+    )
+    b1.ramp2 = pyo.Constraint(
+        expr=b1.next_power_output - b1.power_output <= 100
+    )
 
-    #Add battery integration
-    b1.P_to_battery = pyo.Var(within=pyo.NonNegativeReals,initialize = 0.0)
-    b1.P_to_grid = pyo.Var(within=pyo.NonNegativeReals,initialize = 0.0)
-    b1.P_total = pyo.Constraint(expr = b1.power_output == b1.P_to_battery + b1.P_to_grid)
+    # Add battery integration
+    b1.P_to_battery = pyo.Var(within=pyo.NonNegativeReals, initialize=0.0)
+    b1.P_to_grid = pyo.Var(within=pyo.NonNegativeReals, initialize=0.0)
+    b1.P_total = pyo.Constraint(
+        expr=b1.power_output == b1.P_to_battery + b1.P_to_grid
+    )
 
-    #Simple battery model
+    # Simple battery model
     m.battery = pyo.Block()
-    b2=m.battery
+    b2 = m.battery
 
-    b2.soc = pyo.Var(within=pyo.NonNegativeReals,initialize=0.0, bounds=(0,100))
-    b2.next_soc = pyo.Var(within=pyo.NonNegativeReals,initialize=0.0, bounds=(0,100))
+    b2.soc = pyo.Var(
+        within=pyo.NonNegativeReals, initialize=0.0, bounds=(0, 100)
+    )
+    b2.next_soc = pyo.Var(
+        within=pyo.NonNegativeReals, initialize=0.0, bounds=(0, 100)
+    )
 
-    #Amount discharged to grid this time period (assume discharge is positive)
-    b2.discharge = pyo.Var(initialize = 0.0)
-    b2.energy_change = pyo.Constraint(expr = b2.next_soc == b2.soc - b2.discharge + b1.P_to_battery)
-    b2.energy_down_ramp = pyo.Constraint(expr = b2.soc - b2.next_soc <= 50)
-    b2.energy_up_ramp = pyo.Constraint(expr = b2.next_soc - b2.soc <= 50)
+    # Amount discharged to grid this time period (assume discharge is positive)
+    b2.discharge = pyo.Var(initialize=0.0)
+    b2.energy_change = pyo.Constraint(
+        expr=b2.next_soc == b2.soc - b2.discharge + b1.P_to_battery
+    )
+    b2.energy_down_ramp = pyo.Constraint(expr=b2.soc - b2.next_soc <= 50)
+    b2.energy_up_ramp = pyo.Constraint(expr=b2.next_soc - b2.soc <= 50)
 
-    #Objective function
+    # Objective function
     m.lmp_signal = pyo.Param(initialize=lmp_signal, mutable=True)
     m.revenue = m.lmp_signal * (b1.P_to_grid + b2.discharge)
     m.cost = pyo.Expression(expr=-(m.revenue - b1.fs.operating_cost))
     return m
 
-    # Function to retrieve variable pairs linked between time steps.
-    # This is a user-provided function to a MultiPeriod class
-def get_rankine_link_variable_pairs(b1,b2):
+
+def get_rankine_link_variable_pairs(b1, b2):
     """
+    Function to retrieve variable pairs linked between time steps.
+    This is a user-provided function to a MultiPeriod class
         b1: current time block
         b2: next time block
     """
-    return [(b1.rankine.next_power_output,b2.rankine.power_output),
-            (b1.battery.next_soc,b2.battery.soc)]
+    return [
+        (b1.rankine.next_power_output, b2.rankine.power_output),
+        (b1.battery.next_soc, b2.battery.soc),
+    ]
 
 
 def get_vars_from_block(block):
+    """
+    Obtain all variables in a block.
+    Returns an iterator.
+    """
     seen = set()
     for var in block.component_data_objects(pyo.Var, sort=True):
         if id(var) not in seen:
@@ -150,9 +204,11 @@ def get_vars_from_block(block):
 
 
 def print_model_variables_by_block(par_block, fixed=None):
-    """Print all variables participating in constraints for which
-    the parent block is block."""
-    print('MODEL VARIABLES:')
+    """
+    Print all variables participating in constraints for which
+    the parent block is block.
+    """
+    print("MODEL VARIABLES:")
     for block in set(par_block.component_data_objects(pyo.Block)):
         if block.parent_block() is par_block:
             for v in get_vars_from_block(block):
@@ -162,23 +218,26 @@ def print_model_variables_by_block(par_block, fixed=None):
                 else:
                     print(v.name)
 
-    print('-' * 60)
+    print("-" * 60)
 
 
 def create_mp_rankine_model(n_time_points, lmp_signal):
-    """Create multiperiod rankine model with `n_time_points` hourly
+    """
+    Create multiperiod rankine model with `n_time_points` hourly
     blocks and a given LMP signal (iterable of hourly prices).
     """
     lmp_signal = np.array(lmp_signal)
     assert lmp_signal.size == n_time_points
 
-    mp_rankine = MultiPeriodModel(n_time_points, create_mp_rankine_block,
-                                  get_rankine_link_variable_pairs)
+    mp_rankine = MultiPeriodModel(
+        n_time_points, create_mp_rankine_block, get_rankine_link_variable_pairs
+    )
 
     # data passed to each block time period
     time_points = np.arange(0, n_time_points)
-    data_points = [{"lmp_signal": lmp_signal[idx]}
-                   for idx in range(n_time_points)]
+    data_points = [
+        {"lmp_signal": lmp_signal[idx]} for idx in range(n_time_points)
+    ]
     data_kwargs = dict(zip(time_points, data_points))
 
     # create the multiperiod object
@@ -196,7 +255,7 @@ if __name__ == "__main__":
     #  check that a steady-state block solves
     # ########################################
     b_ss = create_ss_rankine_model()
-    opt = pyo.SolverFactory('ipopt')
+    opt = pyo.SolverFactory("ipopt")
     opt.solve(b_ss)
     opt.solve(b_ss, tee=True)
 
@@ -206,7 +265,7 @@ if __name__ == "__main__":
     b_mp = create_mp_rankine_block(lmp_signal=30.0)
     b_mp.obj = pyo.Objective(expr=b_mp.cost)
     b_mp.battery.soc.fix(0)
-    opt = pyo.SolverFactory('ipopt')
+    opt = pyo.SolverFactory("ipopt")
     opt.solve(b_mp)
     opt.solve(b_mp, tee=True)
 
@@ -222,16 +281,18 @@ if __name__ == "__main__":
     m = mp_rankine.pyomo_model
     blks = mp_rankine.get_active_process_blocks()
     # add_capex_plant(m)
-    import pdb
+
     for blk in blks:
         print(blk.name)
     # get equations and variables for incidence analysis
 
-    from pyomo.core.expr import current as EXPR
-    from pyomo.environ import Constraint
-    import pdb
-    rankine_eqs = [c for c in m.component_data_objects(Constraint, active=True,
-                   descend_into=True) if c.equality]
+    rankine_eqs = [
+        c
+        for c in m.component_data_objects(
+            pyo.Constraint, active=True, descend_into=True
+        )
+        if c.equality
+    ]
 
     rankine_vars = []
     seen = set()
@@ -242,9 +303,8 @@ if __name__ == "__main__":
                 rankine_vars.append(var)
 
     # state_vars = [var for var in rankine_vars if ]
-    print('Number equations:', len(rankine_eqs))
-    print('Number variables:', len(rankine_vars))
-    pdb.set_trace()
+    print("Number equations:", len(rankine_eqs))
+    print("Number variables:", len(rankine_vars))
 
     # print(m.capex_plant.name)
 
@@ -254,23 +314,25 @@ if __name__ == "__main__":
     #  set objective function, fix initial state, and solve
     m.obj = pyo.Objective(expr=sum([blk.cost for blk in blks]))
     blks[0].battery.soc.fix(0)
-    opt = pyo.SolverFactory('ipopt')
+    opt = pyo.SolverFactory("ipopt")
     opt.solve(m, tee=True)
 
     #  check results
     for block in blks:
-        print("Dispatch [MW]: ", pyo.value(block.rankine.power_output),
-              "Charge [MWh]: ", pyo.value(block.battery.soc))
-    pdb.set_trace()
+        print(
+            "Dispatch [MW]: ",
+            pyo.value(block.rankine.power_output),
+            "Charge [MWh]: ",
+            pyo.value(block.battery.soc),
+        )
     print_model_variables_by_block(m)
     m.display()
     for b in mp_rankine.get_active_process_blocks():
         print(b.name)
         b.rankine.display()
-    pdb.set_trace()
 
     # ########################################
-    #  advance time steps -- i.e. deactivate first time period, and add new time 
+    #  advance time steps -- i.e. deactivate first time period, and add new time
     #  period.
     # ########################################
     mp_rankine.advance_time(lmp_signal=0.0)
@@ -279,23 +341,35 @@ if __name__ == "__main__":
     #  Update data (objective price and battery storage level)
     m.obj.expr = sum([blk.cost for blk in blks])
     blks[0].battery.soc.fix(pyo.value(blks[0].battery.soc))
-    blks[0].fix_power = pyo.Constraint(expr=blks[0].rankine.power_output ==
-                                       pyo.value(blks[0].rankine.power_output))
+    blks[0].fix_power = pyo.Constraint(
+        expr=blks[0].rankine.power_output
+        == pyo.value(blks[0].rankine.power_output)
+    )
     opt.solve(m, tee=True)
 
     # Check solution.
     for block in blks:
-        print("Dispatch [MW]: ", pyo.value(block.rankine.power_output),
-              "Charge [MWh]: ", pyo.value(block.battery.soc))
+        print(
+            "Dispatch [MW]: ",
+            pyo.value(block.rankine.power_output),
+            "Charge [MWh]: ",
+            pyo.value(block.battery.soc),
+        )
 
     # advance time again
     mp_rankine.advance_time(lmp_signal=200.0)
     blks = mp_rankine.get_active_process_blocks()
     m.obj.expr = sum([blk.cost for blk in blks])
     blks[0].battery.soc.fix(pyo.value(blks[0].battery.soc))
-    blks[0].fix_power = pyo.Constraint(expr = blks[0].rankine.power_output
-                                        == pyo.value(blks[0].rankine.power_output))
-    opt.solve(m,tee=True)
+    blks[0].fix_power = pyo.Constraint(
+        expr=blks[0].rankine.power_output
+        == pyo.value(blks[0].rankine.power_output)
+    )
+    opt.solve(m, tee=True)
     for block in blks:
-        print("Dispatch [MW]: ", pyo.value(block.rankine.power_output),
-        "Charge [MWh]: ",pyo.value(block.battery.soc))
+        print(
+            "Dispatch [MW]: ",
+            pyo.value(block.rankine.power_output),
+            "Charge [MWh]: ",
+            pyo.value(block.battery.soc),
+        )
--- idaes/apps/multiperiod/examples/multiperiod_rankine_uncertainty.py
+++ idaes/apps/multiperiod/examples/multiperiod_rankine_uncertainty.py
@@ -5,14 +5,20 @@ Created on 2021-11-11 Thu 05:18:42
 
 @author: jasherma
 
-Two-stage RO model of a multiperiod rankine cycle
+Two-stage robust optimization model of a multiperiod rankine cycle.
 """
 
-import multiperiod_rankine_cycle as mc
-import pyomo.environ as pyo
+
 import matplotlib.pyplot as plt
+
+import pyomo.environ as pyo
 import pyomo.contrib.pyros as pyros
-from lmp_uncertainty_models import get_lmp_data, HysterLMPBoxSet
+
+from idaes.apps.multiperiod.examples.lmp_uncertainty_models import (
+    get_lmp_data,
+    HysterLMPBoxSet,
+)
+import idaes.apps.multiperiod.examples.multiperiod_rankine_cycle as mc
 
 
 def plot_results(mp_model, lmp_signal):
@@ -26,33 +32,34 @@ def plot_results(mp_model, lmp_signal):
 
     # get SOC and power output results from model
     soc_list = [pyo.value(blks[i].battery.soc) for i in range(n_time_points)]
-    power_outputs = [pyo.value(blks[i].rankine.power_output)
-                     for i in range(n_time_points)]
+    power_outputs = [
+        pyo.value(blks[i].rankine.power_output) for i in range(n_time_points)
+    ]
 
     fig, ax1 = plt.subplots(figsize=(12, 8))
 
     # plot state of charge
-    ax1.plot(soc_list, 'r')
-    ax1.set_xlabel('Time (hrs)')
-    ax1.set_ylabel('State of Charge (MWh)', color='red')
-    ax1.tick_params(axis='y', labelcolor='red')
+    ax1.plot(soc_list, "r")
+    ax1.set_xlabel("Time (hrs)")
+    ax1.set_ylabel("State of Charge (MWh)", color="red")
+    ax1.tick_params(axis="y", labelcolor="red")
 
     ax2 = ax1.twinx()
-    ax2.set_ylabel('LMP Signal ($/MWh)', color='black')
-    ax2.plot(lmp_signal, 'k')
+    ax2.set_ylabel("LMP Signal ($/MWh)", color="black")
+    ax2.plot(lmp_signal, "k")
     plt.show()
 
     # plot power output
     fig, ax1 = plt.subplots(figsize=(12, 8))
 
-    ax1.plot(power_outputs, 'r')
-    ax1.set_xlabel('Time (hrs)')
-    ax1.set_ylabel('Power Output (MWh)', color='red')
-    ax1.tick_params(axis='y', labelcolor='red')
+    ax1.plot(power_outputs, "r")
+    ax1.set_xlabel("Time (hrs)")
+    ax1.set_ylabel("Power Output (MWh)", color="red")
+    ax1.tick_params(axis="y", labelcolor="red")
 
     ax2 = ax1.twinx()
-    ax2.set_ylabel('LMP Signal ($/MWh)', color='black')
-    ax2.plot(lmp_signal, 'k')
+    ax2.set_ylabel("LMP Signal ($/MWh)", color="black")
+    ax2.plot(lmp_signal, "k")
     plt.show()
 
 
@@ -69,9 +76,15 @@ if __name__ == "__main__":
     lmp_signal = get_lmp_data(n_time_points, start=start)
 
     # construct container for uncertainty set
-    lmp_set = HysterLMPBoxSet(lmp_signal, n_recent, box_growth_rate,
-                              moving_avg_multiplier, hyster_latency,
-                              start_day_hour=0, include_peak_effects=True)
+    lmp_set = HysterLMPBoxSet(
+        lmp_signal,
+        n_recent,
+        box_growth_rate,
+        moving_avg_multiplier,
+        hyster_latency,
+        start_day_hour=0,
+        include_peak_effects=True,
+    )
 
     # create model
     mp_rankine = mc.create_mp_rankine_model(n_time_points, lmp_signal)
@@ -91,14 +104,23 @@ if __name__ == "__main__":
 
     # check results
     for block in blks:
-        print("Dispatch [MW]: ", pyo.value(block.rankine.power_output),
-              "Charge [MWh]: ", pyo.value(block.battery.soc))
+        print(
+            "Dispatch [MW]: ",
+            pyo.value(block.rankine.power_output),
+            "Charge [MWh]: ",
+            pyo.value(block.battery.soc),
+        )
 
     # first and second-stage vars
-    fs_vars = [blks[0].rankine.fs.boiler.inlet.flow_mol[0], 
-               blks[0].rankine.P_to_grid, blks[0].battery.discharge]
-    ss_vars = [blks[i].rankine.fs.boiler.inlet.flow_mol[0]
-               for i in range(1, n_time_points)]
+    fs_vars = [
+        blks[0].rankine.fs.boiler.inlet.flow_mol[0],
+        blks[0].rankine.P_to_grid,
+        blks[0].battery.discharge,
+    ]
+    ss_vars = [
+        blks[i].rankine.fs.boiler.inlet.flow_mol[0]
+        for i in range(1, n_time_points)
+    ]
     ss_vars += [blks[i].rankine.P_to_grid for i in range(1, n_time_points)]
     ss_vars += [blks[i].battery.discharge for i in range(1, n_time_points)]
 
@@ -109,10 +131,12 @@ if __name__ == "__main__":
     from idaes.apps.multiperiod.examples.multiperiod_rankine_cycle import (
         count_degrees_of_freedom,
     )
+
     print(
         "Degrees of Freedom (orig):",
-        count_degrees_of_freedom(mp_rankine.pyomo_model,
-                                 control_vars=fs_vars + ss_vars)
+        count_degrees_of_freedom(
+            mp_rankine.pyomo_model, control_vars=fs_vars + ss_vars
+        ),
     )
 
     # solve with PyROS
@@ -120,9 +144,9 @@ if __name__ == "__main__":
 
     if solve_pyros:
         # solvers for PyROS
-        local_solver = pyo.SolverFactory('ipopt')
-        global_solver = pyo.SolverFactory('ipopt')
-        pyros_solver = pyo.SolverFactory('pyros')
+        local_solver = pyo.SolverFactory("ipopt")
+        global_solver = pyo.SolverFactory("ipopt")
+        pyros_solver = pyo.SolverFactory("pyros")
 
         results = pyros_solver.solve(
             model=mp_rankine.pyomo_model,
@@ -137,5 +161,5 @@ if __name__ == "__main__":
             objective_focus=pyros.ObjectiveType.worst_case,
             tee=False,
             bypass_global_separation=True,
-            subproblem_file_directory='./sublogs/'
+            subproblem_file_directory="./sublogs/",
         )
